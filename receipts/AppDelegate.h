9815ab077581065a46b11172a8171c5ab0ed0dc5
J.K <K_940201@hotmail.com>
2017-08-17 12:18:34 -0700

    Initial Commit

---
+++ receipts/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  receipts
+//
+//  Created by Jaewon Kim on 2017-08-17.
+//  Copyright © 2017 Jaewon Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ receipts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  receipts
+//
+//  Created by Jaewon Kim on 2017-08-17.
+//  Copyright © 2017 Jaewon Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
